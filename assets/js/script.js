2e55863190a73a86cba172532180a16fd5d02792
HaimanotA <haimiayana530@gmail.com>
2022-02-23 13:39:49 +0000

    Add and style a landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,55 @@ function calculateTotalScore(){
   function myFunction() {
 
   
-}
+} **/
+
+const questions = [
+    {
+        "question": "What is the approximate population of Ethiopia in 2022"?",
+        "a": "75 million",
+        "b": "152 million",
+        "c": "120 million",
+        "d": "250 meters",
+        "answer": "c"
+    }, {
+        "question": "When was the Eiffel Tower constructed?",
+        "a": "1954",
+        "b": "1985",
+        "c": "1902",
+        "d": "1889",
+        "answer": "d"
+    }, {
+        "question": "In Notre Dame Cathedral Paris in 1793 _____statues of Biblical kings lost their heads?",
+        "a": "120",
+        "b": "28",
+        "c": "10",
+        "d": "75",
+        "answer": "b"
+    }, {
+        "question": "What is the population of Paris in 2022?",
+        "a": "2.14 million",
+        "b": "1.75 million",
+        "c": "2.50 million",
+        "d": "1.30 million",
+        "answer": "a"
+     }, {
+        "question": "Louvre museum being the most-visited art museum in the world has ________ visitors daily?",
+        "a": "9,000",
+        "b": "3,000",
+        "c": "15,000",
+        "d": "11,000",
+        "answer": "c"
+     }
+]
+
 */
+var questionEl = document.getElementById('question');
+var opt1 = document.getElementById('AnswerA');
+var opt2 = document.getElementById('AnswerB');
+var opt3 = document.getElementById('AnswerC');
+var opt4 = document.getElementById('AnswerD');
+var nextButton = document.getElementById('nextButton');
+var resultCont = document.getElementById('result');
 
 
 function myFunction() {
