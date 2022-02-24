a17270d5a8840f50a2e7e692facebadbeadbb629
HaimanotA <haimiayana530@gmail.com>
2022-02-24 10:45:27 +0000

    Create variable references for html elements in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,22 @@
-/**Declare constants for DOM elements 
+/**Declare constants for DOM elements **/
 
-const questionArea = document.getElementByClassName("container");
-const buttons = document.getElementsByClassName("buttons");
-const showAnswer =document.getElementByClassName("select-btn");
-const score= document.getElementsByClassName("score-area")
-const choices = ["Answer A", "Answer B", "Answer C", "Answer D"];
+const quizContainer = document.getElementById("quiz");
+const resultContainer = document.getElementById("results");
+const submitButton = document.getElementsById("submit");
+const showAnswer =document.getElementById("select-btn")
 
-for (let button of buttons) {
-     button.addEventListener("click", function () {
-       let player=this.getAttribute("data-choice");  
-       playGame(playerChocies);
-     });
+function constructQuiz(){
+  const output = [];
 }
 
-onclick=document.getElementById('select-btn').innerHTML = Correct Answer B. The height of iiii
-
-
-function startQuiz(){
-
-}
-
-
-
-function checkAnswer(){
+function showResults(){
     
 }
 
-function calculateCorrectAnswer(){
-    
-}
+constructQuiz();
 
 
-function displayAnswer(){
-    
-}
 
-
-function incrementScore() {
-    
-}
-
-function incrementWrongAnswer(){
-     Correct Answer: D  The Eiffel Tower was constructed on 1889 for the World Expo.
-
-function calculateTotalScore(){
-  function myFunction() {
-
-  
-} **/
 
 const questions = [
     {
@@ -58,44 +27,36 @@ const questions = [
         "d": "250 meters",
         "answer": "c"
     }, {
-        "question": "When was the Eiffel Tower constructed?",
-        "a": "1954",
-        "b": "1985",
-        "c": "1902",
-        "d": "1889",
+        "question": "How many Ethiopian heritages are registered by UNESCO?",
+        "a": "12",
+        "b": "4",
+        "c": "17",
+        "d": "9",
         "answer": "d"
+    }, {"question":"Which scripts match the plural form of the word "Welcome" in Ge'ez?",
+    "a": "እንጀራ በወጥ",
+    "b": "አብይ አህመድ",
+    "c": "እንኻን ደህና መጡ።",
+    "d": "ኢትዮጵያ",
+    "answer": "c"
+      
     }, {
-        "question": "In Notre Dame Cathedral Paris in 1793 _____statues of Biblical kings lost their heads?",
-        "a": "120",
-        "b": "28",
-        "c": "10",
-        "d": "75",
-        "answer": "b"
-    }, {
-        "question": "What is the population of Paris in 2022?",
-        "a": "2.14 million",
-        "b": "1.75 million",
-        "c": "2.50 million",
-        "d": "1.30 million",
+        "question":"How many languages are spoken in Ethiopia"?",
+        "a": "75-86",
+        "b": "10-14",
+        "c": "6-10",
+        "d": "30-35",
         "answer": "a"
      }, {
-        "question": "Louvre museum being the most-visited art museum in the world has ________ visitors daily?",
-        "a": "9,000",
-        "b": "3,000",
-        "c": "15,000",
-        "d": "11,000",
-        "answer": "c"
+      "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively.Ras Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level"?",
+      "a": "120",
+      "b": "28",
+      "c": "10",
+      "d": "75", 
+      "answer": "b" 
      }
 ]
 
-*/
-var questionEl = document.getElementById('question');
-var opt1 = document.getElementById('AnswerA');
-var opt2 = document.getElementById('AnswerB');
-var opt3 = document.getElementById('AnswerC');
-var opt4 = document.getElementById('AnswerD');
-var nextButton = document.getElementById('nextButton');
-var resultCont = document.getElementById('result');
 
 
 function myFunction() {
@@ -103,3 +64,5 @@ function myFunction() {
 }
 
 
+/**2d : Axum, The Rock-Hewn Churches of Lalibela, Fasil Ghebbi, Simien Mountains National Park, Awash Lower Valley,
+Omo Lower Valley, Tiya **/
