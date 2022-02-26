deed91622b2a51350262da1785c10c58cf399a36
HaimanotA <haimiayana530@gmail.com>
2022-02-26 21:55:03 +0000

    Change background color and images for the landging page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,30 @@
 /**Declare constants for DOM elements **/
 
-const quizContainer = document.getElementById("quiz");
-const resultContainer = document.getElementById("results");
-const submitButton = document.getElementsById("submit");
-const showAnswer =document.getElementById("select-btn")
+const startButton = document.getElementById("start-btn")
+const questionContainerBox = document.getElementById('question-container')
+const questionElement = document.getElementById('questionn')
+const answerButtonsElement = document.getElementById('answer-buttons')
+startButton.addEventListener('click', startGame)
 
-function constructQuiz(){
-  const output = [];
+function startGame() {
+ console.log('Started')
+ startButton.classList.add('hide')
+ questionContainerBox.classList.remove('hide')
+ currentQuestionIndex= 0
+ setNextQuestion()
 }
 
-function showResults(){
-    
+function setNextQuestion () {
+showQuestion(currentQuestionIndex)
 }
 
-constructQuiz();
-
-
-
+function showQuestion(question) { 
+    questionElement.innerText = question.question
+}
 
 const questions = [
     {
-        "question": "What is the approximate population of Ethiopia in 2022"?",
+        "question": "What is the approximate population of Ethiopia in 2022?",
         "a": "75 million",
         "b": "152 million",
         "c": "120 million",
@@ -33,7 +37,7 @@ const questions = [
         "c": "17",
         "d": "9",
         "answer": "d"
-    }, {"question":"Which scripts match the plural form of the word "Welcome" in Ge'ez?",
+    }, {"question":"Which scripts match the plural form of the word Welcome in Ge'ez?",
     "a": "እንጀራ በወጥ",
     "b": "አብይ አህመድ",
     "c": "እንኻን ደህና መጡ።",
@@ -41,14 +45,14 @@ const questions = [
     "answer": "c"
       
     }, {
-        "question":"How many languages are spoken in Ethiopia"?",
+        "question":"How many languages are spoken in Ethiopia?",
         "a": "75-86",
         "b": "10-14",
         "c": "6-10",
         "d": "30-35",
         "answer": "a"
      }, {
-      "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively.Ras Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level"?",
+      "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively.Ras Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level?",
       "a": "120",
       "b": "28",
       "c": "10",
