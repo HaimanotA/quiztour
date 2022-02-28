2c7c054b50c5a98dfab91785ef23200f286f9d5e
HaimanotA <haimiayana530@gmail.com>
2022-02-28 09:25:12 +0000

    Create the build quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,33 @@
-/**Declare constants for DOM elements 
+// Declare constants for DOM elements 
 
-const startButton = document.getElementById("start-btn")
-const questionContainerBox = document.getElementById('question-container')
-const questionElement = document.getElementById('questionn')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById("start-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerElement =document.getElementById("answer-buttons");
+
 startButton.addEventListener('click', startGame)
 
+
 function startGame() {
- console.log('Started')
- startButton.classList.add('hide')
- questionContainerBox.classList.remove('hide')
- currentQuestionIndex= 0
- setNextQuestion()
+  console.log('Started')
+  startButton.classList.add('hide')
+  currentQuestion = questions.sort()
+  currentQuestionIndex=0;
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function setNextQuestion () {
-showQuestion(currentQuestionIndex)
+function setNextQuestion() {
+  showQuestion(currentQuestionIndex[0])
 }
 
-function showQuestion(question) { 
-    questionElement.innerText = question.question
-}. **/
-
-const quizContainer = document.getElementById("quiz");
-const resultContainer = document.getElementById("results");
-const submitButton = document.getElementById("submit");
-const showAnswer =document.getElementById("select-btn");
-
-function constructQuiz(){
-  const output = [];
+function showQuestion(question) {
+ questionElement.innerHTML=question.question
 }
 
 function showResults(){
-    
-}
-
-constructQuiz();
+  questionElement.innerHTML=question.question
+ }
 
 const questions = [
     {
@@ -73,15 +65,5 @@ const questions = [
       "c": "10",
       "d": "75", 
       "answer": "b" 
-     }
-]
-
-
-
-function myFunction() {
-  document.getElementById("select-btn1").innerHTML = "Correct Answer: C  The tower is 324 meteres tall, about the same height as an 81-storey building, and the tallsest structure in Paris.";
-}
-
-
-/**2d : Axum, The Rock-Hewn Churches of Lalibela, Fasil Ghebbi, Simien Mountains National Park, Awash Lower Valley,
-Omo Lower Valley, Tiya **/
+    
+    }]
