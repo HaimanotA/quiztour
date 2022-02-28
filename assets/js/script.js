0ca5da4eb89a03aea94ac68c35044860aa372940
HaimanotA <haimiayana530@gmail.com>
2022-02-28 14:49:40 +0000

    Enable the start button to show  the first question when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,67 +3,88 @@
 const startButton = document.getElementById("start-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
-const answerElement =document.getElementById("answer-buttons");
+const answerElement = document.getElementById("answer-buttons");
+let currentQuestion, currentQuestionIndex;
+const btns = document.querySelectorAll('.btn');
+startButton.addEventListener('click', buildQuiz)
 
-startButton.addEventListener('click', startGame)
 
-
-function startGame() {
+function buildQuiz() {
   console.log('Started')
   startButton.classList.add('hide')
   currentQuestion = questions.sort()
-  currentQuestionIndex=0;
+  currentQuestionIndex = 0;
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
 }
 
 function setNextQuestion() {
-  showQuestion(currentQuestionIndex[0])
+  showQuestion(currentQuestion[currentQuestionIndex])
 }
 
 function showQuestion(question) {
- questionElement.innerHTML=question.question
+  for (const key in question) {
+    questionElement.innerHTML = question.question;
+    btns.forEach(btn => {
+      let choice = btn.dataset.choice;
+      btn.innerHTML = `${question[choice]}`
+      btn.addEventListener('click', function() {
+    
+        if (choice==question.answer) {
+          console.log('correct');
+        }
+        else {
+          console.log('wrong');
+        }
+      });
+    });
+
+  }
+
+ 
+   
+
 }
 
-function showResults(){
-  questionElement.innerHTML=question.question
- }
+function showResults() {
+  questionElement.innerHTML = question.question
+}
 
-const questions = [
-    {
-        "question": "What is the approximate population of Ethiopia in 2022?",
-        "a": "75 million",
-        "b": "152 million",
-        "c": "120 million",
-        "d": "250 meters",
-        "answer": "c"
-    }, {
-        "question": "How many Ethiopian heritages are registered by UNESCO?",
-        "a": "12",
-        "b": "4",
-        "c": "17",
-        "d": "9",
-        "answer": "d"
-    }, {"question":"Which scripts match the plural form of the word Welcome in Ge'ez?",
-    "a": "እንጀራ በወጥ",
-    "b": "አብይ አህመድ",
-    "c": "እንኻን ደህና መጡ።",
-    "d": "ኢትዮጵያ",
-    "answer": "c"
-      
-    }, {
-        "question":"How many languages are spoken in Ethiopia?",
-        "a": "75-86",
-        "b": "10-14",
-        "c": "6-10",
-        "d": "30-35",
-        "answer": "a"
-     }, {
-      "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively.Ras Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level?",
-      "a": "120",
-      "b": "28",
-      "c": "10",
-      "d": "75", 
-      "answer": "b" 
-    
-    }]
+const questions = [{
+  "question": "What is the approximate population of Ethiopia in 2022?",
+  "a": "75 million",
+  "b": "152 million",
+  "c": "120 million",
+  "d": "250 meters",
+  "answer": "c"
+}, {
+  "question": "How many Ethiopian heritages are registered by UNESCO?",
+  "a": "12",
+  "b": "4",
+  "c": "17",
+  "d": "9",
+  "answer": "d"
+}, {
+  "question": "Which scripts match the plural form of the word Welcome in Ge'ez?",
+  "a": "እንጀራ በወጥ",
+  "b": "አብይ አህመድ",
+  "c": "እንኻን ደህና መጡ።",
+  "d": "ኢትዮጵያ",
+  "answer": "c"
+
+}, {
+  "question": "How many languages are spoken in Ethiopia?",
+  "a": "75-86",
+  "b": "10-14",
+  "c": "6-10",
+  "d": "30-35",
+  "answer": "a"
+}, {
+  "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively.Ras Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level?",
+  "a": "120",
+  "b": "28",
+  "c": "10",
+  "d": "75",
+  "answer": "b"
+
+}]
