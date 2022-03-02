ab94844384cfd6931ebfafb269ab11222caf2fd6
HaimanotA <haimiayana530@gmail.com>
2022-03-02 20:21:42 +0000

    Split questions into their own unique file

---
+++ assets/js/questions.js
@@ -0,0 +1,151 @@
+// all questions
+const questions = [
+    {
+        "question": "What is the approximate population of Ethiopia in 2022?",
+        "a": "75 million",
+        "b": "152 million",
+        "c": "120 million",
+        "d": "250 million",
+        "answer": "c"
+    }, {
+        "question": "How many Ethiopian heritages are registered by UNESCO?",
+        "a": "12",
+        "b": "4",
+        "c": "17",
+        "d": "9",
+        "answer": "d"
+    }, {
+        "question": "Which scripts match the plural form of the word Welcome in Ge'ez?",
+        "a": "እንጀራ በወጥ",
+        "b": "አብይ አህመድ",
+        "c": "እንኻን ደህና መጡ።",
+        "d": "ኢትዮጵያ",
+        "answer": "c"
+    }, {
+        "question": "How many languages are spoken in Ethiopia?",
+        "a": "75-86",
+        "b": "10-14",
+        "c": "6-10",
+        "d": "30-35",
+        "answer": "a"
+    }, {
+        "question": "The highest and the lowest point in Ethiopia are called Ras Dejen and Afar Depression respectively. \nRas Dejen is ________meters above from sea level and Afar Depression is ________ meters below sea level?",
+        "a": "1,200 and 76",
+        "b": "4,620 and 125",
+        "c": "5,500 and 30",
+        "d": "3750 and 100",
+        "answer": "b"
+    }, {
+        "question": "Which agricultural product was first harvested in Ethiopia?",
+        "a": "Sunflower Seeds",
+        "b": "Peanuts",
+        "c": "Coffee Beans",
+        "d": "Vanilla Beans",
+        "answer": "c"
+    }, {
+        "question": "Which Ethiopian emperor inspired the creation of the Rastafarian religion?",
+        "a": "Fasilides",
+        "b": "Haile Selassie",
+        "c": "Menelik I",
+        "d": "Yohannes IV",
+        "answer": "b"
+    }, {
+        "question": "Ethiopia was occupied by which European power during the 1930s?",
+        "a": "France",
+        "b": "Italy",
+        "c": "Spain",
+        "d": "Portugal",
+        "answer": "b"
+    }, {
+        "question": "The majority of Ethiopians practice which religion?",
+        "a": "Hinduism",
+        "b": "Islam",
+        "c": "Rastafarianism",
+        "d": "Christianity",
+        "answer": "d"
+    }, {
+        "question": "What type of calendar do Ethiopians follow?",
+        "a": "Gregorian",
+        "b": "Coptic",
+        "c": "Julian",
+        "d": "Lunar",
+        "answer": "b"
+    }, {
+        "question": "What is the name of Ethiopia's currency?",
+        "a": "Metical",
+        "b": "Birr",
+        "c": "Shekel",
+        "d": "Pula",
+        "answer": "b"
+    }, {
+        "question": "Which country broke away from Ethiopia and became independent in 1993?",
+        "a": "Somalia",
+        "b": "Eritra",
+        "c": "Chad",
+        "d": "Djibouti",
+        "answer": "b"
+    }, {
+        "question": "Ethiopia is Africa's top exporter of this product:",
+        "a": "Flour",
+        "b": "Wheat",
+        "c": "Corn",
+        "d": "Sesame Seeds",
+        "answer": "d"
+    }, {
+        "question": "Which town in Ethiopia is famous for its rock-hewn churches.",
+        "a": "Gondar",
+        "b": "Lalibela",
+        "c": "Addis Ababa",
+        "d": "Aksum",
+        "answer": "b"
+    }, {
+        "question": "Injera, a staple of the Ethiopian diet, is a type of what?",
+        "a": "Vegetable",
+        "b": "Bread",
+        "c": "Meat",
+        "d": "Soup",
+        "answer": "b"
+    }, {
+        "question": "What is the official language of Ethiopia?",
+        "a": "Amharic",
+        "b": "Arabic",
+        "c": "Ethiopian",
+        "d": "Swahili",
+        "answer": "a"
+    }, {
+        "question": "What is the capital of Ethiopia?",
+        "a": "Gondar",
+        "b": "Lalibela",
+        "c": "Addis Ababa",
+        "d": "Aksum",
+        "answer": "c"
+    }, {
+        "question": "What does the Green color in the flag represent?",
+        "a": "Peace",
+        "b": "Hope, Justice, and Equality",
+        "c": "Sacrifice for Freedom and Equality",
+        "d": "Labour, Development, and Fertility",
+        "answer": "d"
+    }, {
+        "question": "What does the Yellow color in the flag represent?",
+        "a": "Peace",
+        "b": "Hope, Justice, and Equality",
+        "c": "Sacrifice for Freedom and Equality",
+        "d": "Labour, Development, and Fertility",
+        "answer": "b"
+    }, {
+        "question": "What does the Red color in the flag represent?",
+        "a": "Peace",
+        "b": "Hope, Justice, and Equality",
+        "c": "Sacrifice for Freedom and Equality",
+        "d": "Labour, Development, and Fertility",
+        "answer": "c"
+    }, {
+        "question": "What does the Blue color in the flag represent?",
+        "a": "Peace",
+        "b": "Hope, Justice, and Equality",
+        "c": "Sacrifice for Freedom and Equality",
+        "d": "Labour, Development, and Fertility",
+        "answer": "a"
+    }
+];
