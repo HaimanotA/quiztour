ad8a3b0926e175d3bf32f81f65659ceaf98a318a
HaimanotA <haimiayana530@gmail.com>
2022-03-02 19:56:31 +0000

    Fix quiz to play all questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 /* jshint esversion: 8 */
 
-// Declare variables for DOM elements 
+// declare variables for DOM elements
 const startButton = document.getElementById("start-btn");
-const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerElement = document.getElementById("answer-buttons");
-const restartButton = document.getElementById("restart-btn")
+const restartButton = document.getElementById("restart-btn");
 const resultContainer = document.getElementById('results');
+const btns = document.querySelectorAll('.btn');
 let currentQuestion;
 let currentQuestionIndex = 0;
 let correctAnswers = 0;
-const btns = document.querySelectorAll('.btn');
-let container = document.getElementById("container");
-let body = document.getElementsByTagName("body");
 
 // allow users to click start for the game
 startButton.addEventListener('click', buildQuiz);
@@ -20,36 +17,51 @@ startButton.addEventListener('click', buildQuiz);
 //function to generate the quiz
 function buildQuiz() {
   startButton.classList.add('hide');
+  questionElement.classList.remove('hide');
   answerElement.classList.remove('hide');
   currentQuestion = questions.sort();
-  questionContainerElement.classList.remove('hide');
   setNextQuestion();
 }
 
+// function to set the question
+function setNextQuestion() {
+  if (currentQuestionIndex >= questions.length) {
+    showResults();
+  } else {
+    showQuestion(currentQuestion[currentQuestionIndex]);
+    currentQuestionIndex++;
+  }
+}
 
-
-// function to 
-
+// function to show the next question
 function showQuestion(question) {
-  currentQuestionIndex = currentQuestionIndex + 1;
   questionElement.innerHTML = question.question;
-  const btns = document.querySelectorAll('.btn');
   btns.forEach(btn => {
     let choice = btn.dataset.choice;
     btn.innerHTML = `${question[choice]}`;
-    btn.addEventListener('click', function () {
-      if (choice === question.answer) {
-        correctAnswers = correctAnswers + 1;
-        btn.classList.add('correct');
-        getNextQuestion();
-      } else {
-        btn.classList.add('incorrect');
-        getNextQuestion();
-      }
-    });
   });
 }
 
+// add click event to each choice button
+btns.forEach(btn => {
+  btn.addEventListener('click', function() {
+    checkAnswer(btn, currentQuestion[currentQuestionIndex - 1]);
+  });
+});
+
+// check if the user's choice is correct
+function checkAnswer(btn, question) {
+  let choice = btn.dataset.choice;
+  if (choice === question.answer) {
+    correctAnswers++;
+    btn.classList.add('correct');
+  } else {
+    btn.classList.add('incorrect');
+  }
+  getNextQuestion();
+}
+
+// temporarily disable the choices, and prepare the next question
 function getNextQuestion() {
   btns.forEach(btn => {
     btn.disabled = true;
@@ -58,37 +70,28 @@ function getNextQuestion() {
       btn.disabled = false;
     }, 1000);
   });
-  setNextQuestion()
-}
-
-// function to set the question
-function setNextQuestion() {
-  // let answerElement = document.getElementById("answer-buttons");
-  if (currentQuestionIndex == (questions.length)) {
-    // alert("You have completed the quiz!");
-    questionElement.innerHTML = "Your result is : " +
-      correctAnswers.toString() + "/" + questions.length;
-    answerElement.classList.add('hide');
-    restartButton.classList.remove('hide');
-    // return;
-  }
-  // else {
-  console.log(currentQuestionIndex);
-  // answerElement.replaceWith(answerElement.cloneNode(true));
-  showQuestion(currentQuestion[currentQuestionIndex]);
-  console.log(currentQuestionIndex);
-  // }
+  // delay before moving to next question
+  setTimeout(() => {
+    if (currentQuestionIndex <= questions.length) {
+      setNextQuestion();
+    }
+  }, 1000);
 }
 
 //function to show results
 function showResults() {
-  questionElement.innerHTML = question.question;
+  resultContainer.innerHTML = "Your result is: " + correctAnswers.toString() + " / " + questions.length;
+  answerElement.classList.add('hide');
+  restartButton.classList.remove('hide');
+  questionElement.classList.add('hide');
 }
 
+// event listener for restarting the game
 restartButton.addEventListener('click', function () {
   location.reload();
 });
 
+// all questions
 const questions = [{
   "question": "What is the approximate population of Ethiopia in 2022?",
   "a": "75 million",
@@ -110,7 +113,6 @@ const questions = [{
   "c": "እንኻን ደህና መጡ።",
   "d": "ኢትዮጵያ",
   "answer": "c"
-
 }, {
   "question": "How many languages are spoken in Ethiopia?",
   "a": "75-86",
@@ -125,5 +127,4 @@ const questions = [{
   "c": "5,500 and 30",
   "d": "3750 and 100",
   "answer": "b"
-
 }];
